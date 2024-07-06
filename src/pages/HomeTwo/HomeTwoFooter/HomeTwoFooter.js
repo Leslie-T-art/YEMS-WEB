319b92768297859cdbc6b63608e1598cabffbdad
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-06 17:52:00 +0200

    Update HomeTwoFooter.js

--- src/pages/HomeTwo/HomeTwoFooter/HomeTwoFooter.js
+++ src/pages/HomeTwo/HomeTwoFooter/HomeTwoFooter.js
@@ -15,6 +15,7 @@ const HomeTwoFooter = () => {
                               <img src="img/logo/logo-2.png" alt=""/>
                            </div>
                         </div>
+                        
                         <div className="col-xl-2 col-lg-3 d-none d-lg-block d-xl-block">
                            <div className="footer-subscribe-title">
                               <span>Subscribe to Our Newsletter</span>
@@ -38,82 +39,58 @@ const HomeTwoFooter = () => {
                                        <img src="img/icon/footer-co-icon-1.png" alt=""/>
                                     </div>
                                     <div className="footer-co-content">
-                                       <span>Mon to Fri : 08h30 - 18h00</span>
-                                       <h4>+898 68679 575 09</h4>
+                                       <span>Mon to Fri : 24/7</span>
+                                       <h4>Call 000</h4><br/>
+                                       <h4>Lifeline 131114</h4><br/>
+                                       <h5><span>Crises – you can reach to Yems Holistic approach </span> 1300109344. </h5>
                                     </div>
                                  </div>
                                  <div className="f-contact-info-box fix mb-30">
                                     <div className="footer-co-icon f-left">
-                                       <img src="img/icon/footer-co-icon-2.png" alt=""/>
                                     </div>
-                                    <div className="footer-co-content">
-                                       <span>do you have a question?</span>
-                                       <h4>info@gmail.com</h4>
-                                    </div>
+                                
                                  </div>
                                  <div className="f-contact-info-box fix mb-30">
                                     <div className="footer-co-icon f-left">
-                                       <img src="img/icon/footer-co-icon-2.png" alt=""/>
                                     </div>
-                                    <div className="footer-co-content">
-                                       <span>socials network</span>
-                                       <ul>
-                                          <li><a href="#"><i className="fab fa-facebook-f"></i></a></li>
-                                          <li><a href="#"><i className="fab fa-behance"></i></a></li>
-                                          <li><a href="#"><i className="fab fa-linkedin"></i></a></li>
-                                          <li><a href="#"><i className="fab fa-youtube"></i></a></li>
-                                          <li><a href="#"><i className="fab fa-twitter"></i></a></li>
-                                       </ul>
-                                    </div>
+                                   
                                  </div>
                               </div>
                            </div>
                         </div>
                         <div className="col-xl-4 col-lg-6 col-md-6">
                            <div className="footer-widget mb-40">
-                              <div className="footer-title">
-                                 <h3>Departments</h3>
-                              </div>
+                             
                               <div className="footer-menu footer-menu-2 fix">
-                                 <ul>
-                                    <li><Link to="/servicesDetails">Surgery and Radiology</Link></li>
-                                    <li><Link to="/servicesDetails">Departments</Link></li>
-                                    <li><Link to="/servicesDetails">Family Medicine</Link></li>
-                                    <li><Link to="/doctors">Our Doctors</Link></li>
-                                    <li><Link to="/servicesDetails">Women’s Health</Link></li>
-                                    <li><Link to="/blogs">News</Link></li>
-                                    <li><Link to="/servicesDetails">Optician</Link></li>
-                                    <li><Link to="/shop">Shop</Link></li>
-                                    <li><Link to="/servicesDetails">Pediatrics</Link></li>
-                                    <li><Link to="/contact">Contact Us</Link></li>
-                                    <li><Link to="/servicesDetails">Dermatology</Link></li>
-                                    <li><Link to="/contact">Book an Appointment</Link></li>
-                                 </ul>
+                              <div className="footer-co-content">
+                                       <span>do you have a question?</span>
+                                       <h4>yems@yems.com</h4>
+                                    </div>
                               </div>
                            </div>
                         </div>
                         <div className="col-xl-4 col-md-6">
                            <div className="footer-widget mb-40">
-                              <div className="footer-title">
-                                 <h3>News Feeds</h3>
-                              </div>
-                              <div className="blog-feeds pe-15">
-
-                                 <HomeSingleFooterBlog image="1" />
-                                 <HomeSingleFooterBlog image="2" />
-                                 <HomeSingleFooterBlog image="3" />
-
-                              </div>
+                              
+                              <div className="footer-co-content">
+                                       <span>social network</span>
+                                       <ul>
+                                          <li><a href="#"><i className="fab fa-facebook-f"></i></a></li>
+                                          <li><a href="#"><i className="fab fa-linkedin"></i></a></li>
+                                          <li><a href="#"><i className="fab fa-twitter"></i></a></li>
+                                       </ul>
+                                    </div>
                            </div>
                         </div>
                      </div>
+                     <span>To learn more about how the YEMS Community and Peer cultural Program could help anyone in the community, call YEMS on 1300109344. The YEMS Community Peer Program is available in Brisbane.</span>
                   </div>
                   <div className="footer-bottom-0">
                      <div className="row">
                         <div className="col-xl-12">
                            <div className="footer-copyright-area text-center">
                               <p className="white-color">Copyright ©2022 <strong><a className="white-color"
-                              href="#">Basictheme. </a></strong> All rights reserved</p>
+                              href="#">YEMS. </a></strong> All rights reserved</p>
                            </div>
                         </div>
                      </div>
