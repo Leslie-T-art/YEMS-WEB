28699822805011a9d2cbfa65fb7c3a68170934e7
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-06 17:09:22 +0200

    Update HomeTwoNewsLetter.js

--- src/pages/HomeTwo/HomeTwoNewsLetter/HomeTwoNewsLetter.js
+++ src/pages/HomeTwo/HomeTwoNewsLetter/HomeTwoNewsLetter.js
@@ -9,13 +9,10 @@ const HomeTwoNewsLetter = () => {
                   <div className="col-xl-6 offset-xl-6 col-lg-8 offset-lg-4">
                      <div className="news-letter-box">
                         <div className="about-title news-letter-title mb-20">
-                           <h5 className="pink-color">Subscribe For Get Update</h5>
+                           <h5 className="pink-color">Subscribe To Get Update</h5>
                            <h1>Let’s Find An Office Near You.</h1>
                         </div>
-                        <div className="news-letter-text mb-30">
-                           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eius mod tempor incididunt ut labore et dolore magna
-                              aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
-                        </div>
+                      
                         <div className="subscribe-form">
                            <form action="#">
                               <input type="text" placeholder="Enter your email address" />
