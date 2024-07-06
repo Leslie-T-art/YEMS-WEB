fd3e21396ddc4a9222e9b8cb68db8e8b67fdc5ca
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-06 17:52:25 +0200

    Update SingleBlog2.js

--- src/components/SingleBlog/SingleBlog2.js
+++ src/components/SingleBlog/SingleBlog2.js
@@ -7,14 +7,14 @@ const SingleBlog2 = ({image}) => {
             <div className="col-xl-4 col-lg-6 col-md-6">
                 <div className="latest-news-box latest-news-box-2 mb-30">
                     <div className="latest-news-thumb">
-                        <Link to="/blogDetails"><img src={`img/blog/news-thumb-${image}.jpg`} alt=""/></Link>
+                        <Link to="/"><img src={`img/blog/news-thumb-${image}.jpg`} alt=""/></Link>
                     </div>
                     <div className="latest-news-content-box">
                         <div className="latest-news-content">
                             <div className="news-meta mb-10">
-                                <span><Link to="/blogDetails" className="news-tag">Mental</Link></span>
+                                <span><Link to="/" className="news-tag">Mental</Link></span>
                             </div>
-                            <h3><Link to="/blogDetails">The cure to overthinking.</Link></h3>
+                            <h3><Link to="/">The cure to overthinking.</Link></h3>
                             <p>YEMS has found the cure . This ground breaking discovery was led by a team of professionals ...</p>
                         </div>
                     </div>
