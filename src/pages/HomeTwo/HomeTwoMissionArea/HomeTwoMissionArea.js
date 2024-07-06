5ef10772175606c8d92b7f6c0edf3c8e3d718bdd
Leslie T Chihwai <72800533+Leslie-T-art@users.noreply.github.com>
2024-07-06 15:18:53 +0200

    Update HomeTwoMissionArea.js

--- src/pages/HomeTwo/HomeTwoMissionArea/HomeTwoMissionArea.js
+++ src/pages/HomeTwo/HomeTwoMissionArea/HomeTwoMissionArea.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HomeTwoSingleMission from '../../../components/HomeTwoSingleMission/HomeTwoSingleMission';
+import HomeTwoSingleMission1 from '../../../components/HomeTwoSingleMission/HomeTwoSingleMission1';
 
 const HomeTwoMissionArea = () => {
 
@@ -10,15 +11,15 @@ const HomeTwoMissionArea = () => {
                <div className="row">
                   <div className="col-xl-6 col-lg-10 col-md-12">
                      <div className="about-title mb-20">
-                        <h5 className="pink-color">Our Mission & Vission</h5>
-                        <h1>Keep Going With Your Passion.</h1>
+                        <h5 className="pink-color">   Get Connected </h5>
+                        <h1>Talk To A  Mental Helth Professional.</h1>
                      </div>
                      <div className="about-text mission-about-text">
-                        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p><br/>
+                        <p>•	You may be able to access professional counselling by phone after hours space</p><br/>
                      </div>
                      <div className="mission-vision-list pr-90">
                         <HomeTwoSingleMission icon="1" />
-                        <HomeTwoSingleMission icon="1" border="border-0" />
+                        <HomeTwoSingleMission1 icon="1" border="border-0" />
                      </div>
                   </div>
                   <div className="col-xl-6 col-lg-6 d-lg-none d-xl-block">
